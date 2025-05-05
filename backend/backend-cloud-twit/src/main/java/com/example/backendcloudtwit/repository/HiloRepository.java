5d94e635d71bbb9a68ebe7095577483a035a8d64
IsaacGamero <isaac.gamero@utec.edu.pe>
2025-05-05 09:53:32 -0500

    Add files via upload

---
+++ backend/backend-cloud-twit/src/main/java/com/example/backendcloudtwit/repository/HiloRepository.java
@@ -0,0 +1,13 @@
+package com.example.backendcloudtwit.repository;
+
+import com.example.backendcloudtwit.model.Hilo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HiloRepository extends JpaRepository<Hilo, Long> {
+    List<Hilo> findByPostId(Long postId);
+}
+
