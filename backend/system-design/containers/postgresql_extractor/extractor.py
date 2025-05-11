8d229ed01cb4cb0da5ce6552f0b423a98a0d45ae
Alexander Morales-Panitz <alexander@moralespanitz.com>
2025-05-10 21:21:03 -0500

    refactor: changes env in extractor

--- backend/system-design/containers/postgresql_extractor/extractor.py
+++ backend/system-design/containers/postgresql_extractor/extractor.py
@@ -1,7 +1,7 @@
 import psycopg2
 import pandas as pd
 import os
-# import boto3
+import boto3
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,8 +13,8 @@ PG_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 PG_DATABASE = os.environ.get("POSTGRES_DB")
 
 # --- Configuración de S3 ---
-# S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "your-s3-bucket-name")
-# S3_PREFIX = os.environ.get("S3_PREFIX", "ingesta/postgres")
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+S3_PREFIX = os.environ.get("S3_PREFIX")
 OUTPUT_DIR = "./output"
 
 def extraer_y_cargar_postgresql():
@@ -31,7 +31,7 @@ def extraer_y_cargar_postgresql():
         cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
         tables = [table[0] for table in cursor.fetchall()]
 
-        # s3_client = boto3.client('s3')
+        s3_client = boto3.client('s3')
 
         for table_name in tables:
             print(f"Extrayendo datos de la tabla: {table_name}")
@@ -45,15 +45,15 @@ def extraer_y_cargar_postgresql():
             df.to_csv(output_file, index=False, encoding='utf-8')
             print(f"Guardado localmente en: {output_file}")
 
-            # # --- Cargar a S3 ---
-            # s3_key = f"{S3_PREFIX}/{table_name}.csv"
-            # try:
-            #     s3_client.upload_file(output_file, S3_BUCKET_NAME, s3_key)
-            #     print(f"Cargado a s3://{S3_BUCKET_NAME}/{s3_key}")
-            #     os.remove(output_file) # Opcional: Eliminar el archivo local
-            #     print(f"Archivo local {output_file} eliminado.")
-            # except Exception as e:
-            #     print(f"Error al cargar {output_file} a S3: {e}")
+            # --- Cargar a S3 ---
+            s3_key = f"{S3_PREFIX}/{table_name}.csv"
+            try:
+                s3_client.upload_file(output_file, S3_BUCKET_NAME, s3_key)
+                print(f"Cargado a s3://{S3_BUCKET_NAME}/{s3_key}")
+                os.remove(output_file) # Opcional: Eliminar el archivo local
+                print(f"Archivo local {output_file} eliminado.")
+            except Exception as e:
+                print(f"Error al cargar {output_file} a S3: {e}")
 
         conn.commit()
 
