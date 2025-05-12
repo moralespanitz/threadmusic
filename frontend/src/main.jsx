a9afecb0634d83afa51457008fa648eb6d526011
JuanTorres7 <juan.torres.f@utec.edu.pe>
2025-05-11 21:01:13 -0500

    Frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,21 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ApolloProvider } from '@apollo/client';
 import client from './apolloClient.js';
+import { ClerkProvider } from '@clerk/clerk-react';
 
 import './styles/index.css'
 import App from './App.jsx'
 
+const clerkPubKey = 'pk_test_cHJlc2VudC1wb3Jwb2lzZS05NS5jbGVyay5hY2NvdW50cy5kZXYk';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ApolloProvider>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ApolloProvider>
+    </ClerkProvider>
   </React.StrictMode>
 )
